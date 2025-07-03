89cca6e6e58df61e6581ce298768141ea38bb53d
MatissimeVelvet <Devastator@gmx.it>
2025-07-03 08:05:19 +0800

    Update cli.sh

--- AWS/cli.sh
+++ AWS/cli.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+# 完全无交互地更新并升级
+DEBIAN_FRONTEND=noninteractive apt-get update -qq && \
+DEBIAN_FRONTEND=noninteractive apt-get -y upgrade \
+    -o Dpkg::Options::="--force-confdef" \
+    -o Dpkg::Options::="--force-confold"
+
+mkdir /root/AWS
+cd /root/AWS
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
