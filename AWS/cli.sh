85c68c4f09fed33bf0c8e258b01f97037c9ddf2e
MatissimeVelvet <Devastator@gmx.it>
2025-07-03 08:06:04 +0800

    Update cli.sh

--- AWS/cli.sh
+++ AWS/cli.sh
@@ -4,6 +4,7 @@ DEBIAN_FRONTEND=noninteractive apt-get update -qq && \
 DEBIAN_FRONTEND=noninteractive apt-get -y upgrade \
     -o Dpkg::Options::="--force-confdef" \
     -o Dpkg::Options::="--force-confold"
+apt -y install curl nload rsync wget unzip p7zip
 
 mkdir /root/AWS
 cd /root/AWS
